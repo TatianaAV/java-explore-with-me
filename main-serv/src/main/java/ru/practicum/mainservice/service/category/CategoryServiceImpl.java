f783ee90499fc7d4ae50dd3b09139501b4d49b7d
TatianaAV <tavolnova@gmail.com>
2023-05-13 19:05:31 +0300

    fix : pom ${project.version}

--- main-serv/src/main/java/ru/practicum/mainservice/service/category/CategoryServiceImpl.java
+++ main-serv/src/main/java/ru/practicum/mainservice/service/category/CategoryServiceImpl.java
@@ -50,7 +50,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public CategoryDto updateCategory(Long catId, NewCategoryDto updateCategory) {
         log.info("PATCH AdminController/ admin/categories/{}, {}", catId, updateCategory);
-        Category findCategory = categoryRepository.findById(catId).orElseThrow(() -> new NotFoundException(""));
+        Category findCategory = categoryRepository.findById(catId).orElseThrow(() -> new NotFoundException("Not found category"));
         findCategory.setName(updateCategory.getName());
         return categoryMapper.toCategoryDto(findCategory);
     }
